857fb322491dd15f41bb9eb54157a3c3a23cee15
Aryan Eftekhari <aryan.eftekhari@gmail.com>
2020-12-09 21:01:57 +0100

    update

---
+++ R/SQUIC.R
@@ -0,0 +1,130 @@
+usethis::use_package("Matrix") 
+usethis::use_package("MASS") # Use for multivriate data generation
+usethis::use_package("BigQuic") # Use for multivriate data generation
+
+#usethis::use_package("https://cran.r-project.org/src/contrib/Archive/QUIC/QUIC_1.1.1.tar.gz", type="source")
+
+SQUIC <- function(Data, M, lambda, max_iter, drop_tol, term_tol, X0, W0) {
+  
+  mode_M<-1;
+  mode_X0W0<-1;
+  if(is.null(M)){
+      M<-Matrix::Diagonal(1);
+      mode_M<-0;
+    }
+  if(is.null(X0) || is.null(W0)){
+	X0<-Matrix::Diagonal(1);
+	W0<-Matrix::Diagonal(1);
+    mode_X0W0<-0;
+  }
+  
+   .Call(`_SQUIC_SQUIC_BASE`, mode_M, mode_X0W0, Data, M, lambda, max_iter, drop_tol, term_tol, X0, W0);
+}
+
+
+SQUIC_demo_iid <- function(p=100, n=20, lambda=.5,max_iter=10,eps=1e-3) {
+
+	sprintf("Generating data for test started");
+
+	start_time <- Sys.time()
+	Y=matrix( rnorm(p*p,mean<-0,sd<-1), p, n);
+	end_time=Sys.time()
+
+	sprintf("Generating data for test finished: time=%f",end_time-start_time);
+
+	ouput<-SQUIC(Y, NULL, lambda, max_iter, drop_tol=1e-4, term_tol=eps, NULL, NULL);
+
+	return(ouput);
+}
+
+
+SQUIC_demo_trid <- function(p=100, n=20, lambda=.5, M=NULL, max_iter=10,eps=1e-3) {
+
+	sprintf("Generating data for test started");
+
+	start_time<-Sys.time()
+	iC_star <- Matrix::bandSparse(p, p,
+                (-1):1,
+                list(rep(-0.5, p-1), 
+                     rep(1.25, p), 
+                     rep(-0.5, p-1)));
+
+	C_star <- solve(iC_star);
+	mu_star <- replicate(p, 0);
+ 	Data <- MASS::mvrnorm(n, mu_star, C_star, tol = 1e-6, empirical = FALSE, EISPACK = FALSE);
+ 	Data <- t(Data);
+ 	end_time <- Sys.time()
+
+	sprintf("Generating data for test finished: time=%f",end_time-start_time);
+
+	drop_tol <- eps;
+	term_tol <- eps;
+	ouput <- SQUIC(Data, M, lambda, max_iter, drop_tol=1e-4, term_tol, NULL, NULL);
+	return(ouput);
+}
+
+
+SQUIC_demo_benchmark <- function(p_power_max,lambda=0.5,n=100,eps=1e-3,max_iter=10) {
+
+	time_squic<-replicate(p_power_max, 0);
+	time_bigquic<-replicate(p_power_max, 0);
+
+	error_iC_1<-replicate(p_power_max, 0);
+	error_C_1<-replicate(p_power_max, 0);
+
+	error_iC_0<-replicate(p_power_max, 0);
+	error_C_0<-replicate(p_power_max, 0);
+
+	for (i in 1:p_power_max) {
+
+		p <- 2^i;
+
+		sprintf("Generating data for test started for p=%i",p);
+
+		start_time <- Sys.time()
+		iC_star <- Matrix::bandSparse(p, p,
+	                (-1):1,
+	                list(rep(-0.5, p-1), 
+	                     rep(1.25, p), 
+	                     rep(-0.5, p-1)));
+
+		C_star <- solve(iC_star);
+		mu_star <- replicate(p, 0);
+	 	Data <- MASS::mvrnorm(n, mu_star, C_star, tol = 1e-6, empirical = FALSE, EISPACK = FALSE);
+	 	Data_t <- t(Data);
+	 	end_time <- Sys.time()
+
+		sprintf("Generating data for test finished: time=%f",end_time-start_time);
+
+		bigquic_start_time <- Sys.time()
+		#bigquic_output<-BigQuic::BigQuic(X = Data, inputFileName = NULL, outputFileName = NULL, lambda = lambda, numthreads = 8, maxit = max_iter, epsilon = eps, k = 0, memory_size = 8000, verbose = 1, isnormalized = 0, seed = NULL, use_ram = TRUE);
+		bigquic_output<-QUIC::QUIC(S=cov(Data), rho=lambda, path = NULL, tol = eps, msg = 1, maxIter = max_iter, X.init =NULL, W.init = NULL)
+		bigquic_end_time <- Sys.time()
+
+		squic_start_time <- Sys.time()
+		squic_output<-SQUIC(Data_t, NULL, lambda=lambda, max_iter=max_iter, drop_tol=1e-4, term_tol=eps, NULL, NULL);
+		squic_end_time <- Sys.time()
+
+		time_squic[i]<-squic_end_time-squic_start_time;
+		time_bigquic[i]<-bigquic_end_time-bigquic_start_time;
+
+		# L1 per row error with respect to the QUIC
+		error_iC_1[i]<-Matrix::norm(squic_output$iC-bigquic_output$X,"1")/p;
+		error_C_1[i]<-Matrix::norm(squic_output$C-bigquic_output$W,"1")/p;
+
+		# Measure of the difference of the nonzero pattern ... someting like the L0 nrom
+		error_iC_0[i]<-Matrix::norm( (squic_output$iC!=0)-1*(bigquic_output$X!=0),"1")/p;
+		error_C_0[i]<-Matrix::norm( (squic_output$C!=0)-1*(bigquic_output$W!=0),"1")/p;
+	}
+
+	output <- list(
+		"time_squic"   = time_squic, 
+		"time_bigquic" = time_bigquic, 
+		"error_iC_1"   = error_iC_1,
+		"error_C_1"    = error_C_1,
+		"error_iC_0"   = error_iC_0,
+		"error_C_0"    = error_C_0		
+		)
+
+	return(output);
+}
---
+++ src/SQUIC_BASE.cpp
@@ -0,0 +1,264 @@
+// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
+// we only include RcppArmadillo.h which pulls Rcpp.h in for us
+#include "RcppArmadillo.h"
+#include <stdio.h>
+#include <assert.h>
+// [[Rcpp::depends(RcppArmadillo)]]
+// [[Rcpp::plugins(cpp11)]]
+
+
+
+// SQUIC is fixed with type: long in (This is a requirement for Cholmod)
+#define integer long
+// SQUIC Library iterface
+extern "C" {
+    void SQUIC_C(
+        // Inputs
+        integer p, integer n, double *Y,
+        double lambda,
+        integer *M_i, integer *M_j, double *M_val, integer M_nnz,
+        int max_iter, double drop_tol, double term_tol, int verbose, bool matrix_format_coo,
+        // input/output
+        integer *&X_i, integer *&X_j, double *&X_val, integer &X_nnz,
+        integer *&W_i, integer *&W_j, double *&W_val, integer &W_nnz,
+        //outputs
+        int &info_num_iter,
+        double *info_times,     // length must be 6: [total_time,cov_time,itr_cumtime,chol_cumtime,inv_cumtime,upd_cumtime]
+        double *info_objective, // length must be size max_iter
+        double &info_dgap,
+        double &info_logdetx);
+}
+
+using namespace Rcpp;
+using namespace arma;
+
+
+// [[Rcpp::export]]
+List SQUIC_BASE(int mode_M, int mode_X0W0, arma::mat & Data, arma::sp_mat & M, double lambda, int max_iter, double drop_tol, double term_tol, arma::sp_mat & X0, arma::sp_mat & W0) {
+
+    integer p = Data.n_rows;
+    integer n = Data.n_cols;
+
+    // Matricies must be the correct dimension
+    if (p < 2) {
+        stop(" The number of random variables 'p' must be larger than 1");
+    }
+
+    // Make Matricies
+    integer* M_i;
+    integer* M_j;
+    double* M_val;
+    integer M_nnz = -1;
+
+    integer* X_i;
+    integer* X_j;
+    double* X_val;
+    integer X_nnz = -1;
+
+    integer* W_i;
+    integer* W_j;
+    double* W_val;
+    integer W_nnz = -1;
+
+
+    // mode_M==TRUE : M is provided
+    if (mode_M == 1) {
+
+        M.sync();
+
+        // Matricies must be the correct dimension
+        if (p != M.n_rows && p != M.n_cols ) {
+            stop("Matrix M must be of size (pxp) p=%d", p);
+        }
+
+
+        M_nnz = M.n_nonzero;
+        auto M_row_indices = arma::access::rwp(M.row_indices);
+        auto M_col_ptrs = arma::access::rwp(M.col_ptrs);
+        auto M_values = arma::access::rwp(M.values);
+
+        M_i  = new integer[M_nnz];
+        M_j  = new integer[p + 1];
+        M_val = new double[M_nnz];
+
+        for (integer i = 0; i < M_nnz; ++i) {
+            M_i[i] = M_row_indices[i];
+            M_val[i] = M_values[i];
+        }
+
+        for (integer i = 0; i < p + 1; ++i) {
+            M_j[i] = M_col_ptrs[i];
+        }
+    }
+
+
+
+    // Mode[1]==TRUE: Both X0 or W0 are provided
+    if (mode_X0W0 == 1) {
+
+        X0.sync();
+        W0.sync();
+        X_nnz = X0.n_nonzero;
+        W_nnz = W0.n_nonzero;
+
+        // ERROR CHECKS
+        {
+            // Matricies must be the correct dimension
+            if (p != X0.n_rows && p != X0.n_cols ) {
+                if (mode_M) {
+                    delete[] M_i;
+                    delete[] M_j;
+                    delete[] M_val;
+                }
+                stop("Matrix X0 must be of size (pxp) p=%d.", p);
+            }
+            if (p != W0.n_rows && p != W0.n_cols ) {
+                if (mode_M) {
+                    delete[] M_i;
+                    delete[] M_j;
+                    delete[] M_val;
+                }
+                stop("Matrix X0 must be of size (pxp) p=%d.", p);
+            }
+
+            if (X_nnz < p) {
+                if (mode_M) {
+                    delete[] M_i;
+                    delete[] M_j;
+                    delete[] M_val;
+                }
+                stop("Matrix X0 must be at least diagional (X0.nnz >= p).");
+            }
+
+            if (W_nnz < p) {
+                if (mode_M) {
+                    delete[] M_i;
+                    delete[] M_j;
+                    delete[] M_val;
+                }
+                stop("Matrix W0 must be at least diagional (W0.nnz >= p).");
+            }
+        }
+
+
+        // X0 data structure
+        auto X_row_indices = arma::access::rwp(X0.row_indices);
+        auto X_col_ptrs = arma::access::rwp(X0.col_ptrs);
+        auto X_values = arma::access::rwp(X0.values);
+
+        X_i  = new integer[X_nnz];
+        X_j  = new integer[p + 1];
+        X_val = new double[X_nnz];
+
+        for (integer i = 0; i < X_nnz; ++i) {
+            X_i[i] = X_row_indices[i];
+            X_val[i] = X_values[i];
+        }
+
+        for (integer i = 0; i < p + 1; ++i) {
+            X_j[i] = X_col_ptrs[i];
+        }
+
+
+        // W0 data structure
+        auto W_row_indices = arma::access::rwp(W0.row_indices);
+        auto W_col_ptrs = arma::access::rwp(W0.col_ptrs);
+        auto W_values = arma::access::rwp(W0.values);
+
+        W_i  = new integer[W_nnz];
+        W_j  = new integer[p + 1];
+        W_val = new double[W_nnz];
+
+        for (integer i = 0; i < W_nnz; ++i) {
+            W_i[i] = W_row_indices[i];
+            W_val[i] = W_values[i];
+        }
+
+        for (integer i = 0; i < p + 1; ++i) {
+            W_j[i] = W_col_ptrs[i];
+        }
+    }
+
+    int info_num_iter = -1;
+    double info_dgap;
+    double info_logdetx;
+    arma::Col<double> info_times(6);
+    info_times.fill(-1.0);
+    arma::Col<double> info_objective(max_iter);
+    info_objective.fill(-1.0);
+
+
+    // hard code this for now ...
+    int verbose = 1;
+
+
+    SQUIC_C(
+        p, n, Data.memptr(),
+        lambda,
+        M_i, M_j, M_val, M_nnz,
+        max_iter, drop_tol, term_tol, verbose, false,
+        X_i, X_j, X_val, X_nnz,
+        W_i, W_j, W_val, W_nnz,
+        info_num_iter,
+        info_times.memptr(),
+        info_objective.memptr(),
+        info_dgap,
+        info_logdetx
+    );
+
+    // Delete the number o f
+    info_objective.resize(info_num_iter);
+
+    // create sp_mat object of appropriate size
+    // In order to access the internal arrays of the SpMat class call .sync()
+    arma::SpMat<double> iC(p, p);
+    arma::SpMat<double> C(p, p);
+    iC.sync();
+    C.sync();
+
+    // Making space for the elements
+    iC.mem_resize(X_nnz);
+    C.mem_resize(W_nnz);
+
+    // Copying elements
+    std::copy(X_i  , X_i + X_nnz  , arma::access::rwp(iC.row_indices));
+    std::copy(X_j  , X_j + p + 1  , arma::access::rwp(iC.col_ptrs));
+    std::copy(X_val, X_val + X_nnz, arma::access::rwp(iC.values));
+
+    std::copy(W_i  , W_i + W_nnz  , arma::access::rwp(C.row_indices));
+    std::copy(W_j  , W_j + p + 1  , arma::access::rwp(C.col_ptrs));
+    std::copy(W_val, W_val + W_nnz, arma::access::rwp(C.values));
+
+    // Delete Buffers
+    if (mode_M == true) {
+        delete[] M_i;
+        delete[] M_j;
+        delete[] M_val;
+
+    }
+
+    if (mode_X0W0 == true) {
+        delete[] X_i;
+        delete[] X_j;
+        delete[] X_val;
+
+        delete[] W_i;
+        delete[] W_j;
+        delete[] W_val;
+    }
+
+    return Rcpp::List::create(
+               Named("iC") = iC,
+               Named("C") = C,
+               Named("info_time_total")       = info_times[0],
+               Named("info_time_impcov")      = info_times[1],
+               Named("info_time_optimz")      = info_times[2],
+               Named("info_time_factor")      = info_times[3],
+               Named("info_time_aprinv")      = info_times[4],
+               Named("info_time_updte")       = info_times[5],
+               Named("info_objective")        = info_objective,
+               Named("info_dgap")             = info_dgap,
+               Named("info_logdetx")          = info_logdetx
+           );
+}
+
