0d61047da84017ac21917f82ca0fcd05e35c029d
Aryan Eftekhari <aryan.eftekhari@gmail.com>
2021-01-26 07:17:58 +0100

    udpate

--- R/demo.R
+++ R/demo.R
@@ -109,7 +109,7 @@ DEMO.load_data<-function(type="trid",p=4^5,n=100)
 }
 
 
-DEMO.lambda_search<- function(type="trid", p=4^5 , n=100 , lambda_sample=.4){
+DEMO.lambda_search<- function(type="trid", p=4^5 ,lambda_sample=.3){
 
   	# Generate data
 	out<-SQUIC::DEMO.load_data(type=type , p=p ,n=n );
@@ -126,17 +126,19 @@ DEMO.lambda_search<- function(type="trid", p=4^5 , n=100 , lambda_sample=.4){
 	out<-SQUIC::SQUIC_CV(data=data , lambda_set=lambda_set );
 	print("SQUIC::SQUIC_CV AIC");
 	print(out);
-	lambda_opt=out$lambda_opt;
+	lambda_opt <- out$lambda_opt;
+	CV_mean    <- out$CV_mean;
 
-	f1_set	<-replicate(length(lambda_set), 0);
-	acc_set <-replicate(length(lambda_set), 0);
+
+	f1_set	    <-replicate(length(lambda_set), 0);
+	acc_set     <-replicate(length(lambda_set), 0);
 	nnzpr_X_set <-replicate(length(lambda_set), 0);	
 	
 	for (i in 1:length(lambda_set)) {
 		out<-SQUIC::DEMO.compare(alg=alg , data=data , lambda=lambda_set[i] , tol=1e-3 , max_iter=5 , X_star=X_star);
-		f1_set[i]<-out$f1;
-		acc_set[i]<-out$acc;
-		nnzpr_X_set[i]<- (Matrix::nnzero(out$X)/p);
+		f1_set[i]      <-out$f1;
+		acc_set[i]     <-out$acc;
+		nnzpr_X_set[i] <- (Matrix::nnzero(out$X)/p);
 	}
 
 	output <- list(
@@ -144,7 +146,8 @@ DEMO.lambda_search<- function(type="trid", p=4^5 , n=100 , lambda_sample=.4){
 		"f1_set"     	 = f1_set, 
 		"acc_set"    	 = acc_set,
 		"lambda_opt"     = lambda_opt,				
-		"lambda_set" 	 = lambda_set
+		"lambda_set" 	 = lambda_set,
+		"CV_mean"        =CV_mean
 	);
 
 	return(output);
